92ca36b7832ef3ec04171dbd6370dc6703e8a0d1
martinjulian <martinjulianramos@gmail.com>
2021-10-05 20:51:50 -0300

    terminado ejericio 6 de collections

--- src/main/java/ejercicios/ConditionalExercisesCollections2.java
+++ src/main/java/ejercicios/ConditionalExercisesCollections2.java
@@ -13,9 +13,9 @@ public class ConditionalExercisesCollections2 {
         exercises.addItem("Ejercicio 1 ", ConditionalExercisesCollections2::exercise1);
         exercises.addItem("Ejercicio 2 ", ConditionalExercisesCollections2::exercise2);
         exercises.addItem("Ejercicio 3 ", ConditionalExercisesCollections2::exercise3);
-        /*exercises.addItem("Ejercicio 4 ", ConditionalExercisesCollections2::exercise4);
-        exercises.addItem("Ejercicio 5 ", ConditionalExercisesCollections21::exercise5);
-        exercises.addItem("Ejercicio 6 ", ConditionalExercisesCollections2::exercise6);
+        exercises.addItem("Ejercicio 4 ", ConditionalExercisesCollections2::exercise4);
+        exercises.addItem("Ejercicio 5 ", ConditionalExercisesCollections2::exercise5);
+        /*exercises.addItem("Ejercicio 6 ", ConditionalExercisesCollections2::exercise6);
         exercises.addItem("Ejercicio 7 ", ConditionalExercisesCollections2::exercise7);
         exercises.addItem("Ejercicio 8 ", ConditionalExercisesCollections2::exercise8);*/
         }
@@ -51,7 +51,7 @@ public class ConditionalExercisesCollections2 {
         List<Integer> list2 = createListOfNumbers(5, "lista 2");
 
         Collections.sort(list1);
-        Collections.sort(list2, Collections.reverseOrder());
+        list2.sort(Collections.reverseOrder());
 
         for (int i = 0; i < 5; i++ ){
             print(list1.get(i) + "-" + list2.get(i));
@@ -81,32 +81,89 @@ public class ConditionalExercisesCollections2 {
         final String team3Name = stringInput("Ingrese el nombre del tercer equipo");
         final int team3Score = intInput("Ingrese el puntaje del tercer equipo");
 
-        Map<Integer, String > teamByScore = new TreeMap<>();
+        Map<Integer, String > teamByScore = new HashMap<>();
         teamByScore.put(team1Score,team1Name);
         teamByScore.put(team2Score,team2Name);
         teamByScore.put(team3Score,team3Name);
 
-        Map<Integer, String> sortedMap = new TreeMap<>(Collections.reverseOrder());
-        sortedMap.putAll(teamByScore);
-
         if (teamByScore.size() < 3) {
             print("¡Aún no hay un podio definido!");
 
-            //aca papucho
-        } else {
-            final Map<Podium, String> podiumMap = new HashMap<>();
-            podiumMap.put(Podium.CHAMPION, sortedMap.get(0));
-            podiumMap.put(Podium.SUB_CHAMPION, sortedMap.get(1));
-            podiumMap.put(Podium.THIRD_PLACE, sortedMap.get(2));
 
-           print(podiumMap);
+        } else{
+            final List<Integer> sortedScores = new ArrayList<>();
+            sortedScores.add(team1Score);
+            sortedScores.add(team2Score);
+            sortedScores.add(team3Score);
+
+            sortedScores.sort(Collections.reverseOrder());
+
+        final Map<Podium, String> podiumMap = new LinkedHashMap<>();
+        podiumMap.put(Podium.CHAMPION, teamByScore.get(sortedScores.get(0)));
+        podiumMap.put(Podium.SUB_CHAMPION, teamByScore.get(sortedScores.get(1)));
+        podiumMap.put(Podium.THIRD_PLACE, teamByScore.get(sortedScores.get(2)));
+
+        podiumMap.forEach((k, v) -> print(String.format("%s-%s", k, v)));
         }
-    }
-    private enum Podium
-    {
-        CHAMPION, SUB_CHAMPION, THIRD_PLACE;
+
     }
 
+     private enum Podium
+    {
+        CHAMPION, SUB_CHAMPION, THIRD_PLACE,
+    }
+   /* 4 - Sin ingreso de usuario. Armar un mapa con las siguientes ligas de fútbol y sus equipos
+    más importantes (mínimo 2, máximo 4): "premier-league", "serie-a", "la-liga",
+            "primera-división".
+    Imprimir con el siguiente formato:
+    Equipos más representativos de premier-league:
+    Manchester City, Manchester Utd, Chelsea, Liverpool*/
+    public static void exercise4() {
+            final Map<League, List<String>> teamsByLeague = new HashMap<>();
+
+            final List<String>  premierLeagueTeams = new ArrayList<>();
+            premierLeagueTeams.add("Manchester City");
+            premierLeagueTeams.add("Manchester Utd");
+            premierLeagueTeams.add("Chelsea");
+            premierLeagueTeams.add("Liverpool");
+            teamsByLeague.put(League.PREMIER_LEAGUE, premierLeagueTeams);
+
+            final List<String>  serieATeams = new ArrayList<>();
+            serieATeams.add("Milan");
+            serieATeams.add("Juventus");
+            serieATeams.add("Inter");
+            serieATeams.add("Napoli");
+            teamsByLeague.put(League.SERIE_A, serieATeams);
+
+            final List<String>  laLigaTeams = new ArrayList<>();
+            laLigaTeams.add("Barcelona");
+            laLigaTeams.add("Real Madrid");
+            laLigaTeams.add("Atletico madrid");
+            laLigaTeams.add("Sevilla");
+            teamsByLeague.put(League.LA_LIGA, laLigaTeams);
+
+            final List<String>  primeraDivisionTeams = new ArrayList<>();
+            primeraDivisionTeams.add("Boca");
+            primeraDivisionTeams.add("River");
+            primeraDivisionTeams.add("Racing");
+            primeraDivisionTeams.add("Independiente");
+            teamsByLeague.put(League.PRIMERA_DIVISION, primeraDivisionTeams);
+
+            teamsByLeague.forEach((League, teams) -> {
+            final String teamsJoined= String.join( ", ", teams );
+
+                print("Equipos más representativos de " + League + ":");
+                print(teamsJoined);
+            });
+    }
+    private enum League {
+        PREMIER_LEAGUE, SERIE_A, LA_LIGA, PRIMERA_DIVISION
+    }
+    /*5 - Resolver ejercicio de colores con Collections y sin usar if, salvo para validación de
+    errores y combinación inexistente (Marrón). Recomendado: usar Map*/
+    public static void exercise5() {
+
+    }
 }
 
 
